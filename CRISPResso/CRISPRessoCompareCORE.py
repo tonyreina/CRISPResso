8c573e89ad8ed6e8f1936859571136dfa189427c
Tony Reina <37851530+tonyreina@users.noreply.github.com>
2023-02-14 17:07:38 -0800

    Update CRISPRessoCompareCORE.py

--- CRISPResso/CRISPRessoCompareCORE.py
+++ CRISPResso/CRISPRessoCompareCORE.py
@@ -8,9 +8,12 @@ import pickle as cp
 
 from scipy.stats import mannwhitneyu, ttest_ind
 
-
-
 import logging
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+import pandas as pd
 
 logging.basicConfig(
     level=logging.INFO,
@@ -261,7 +264,6 @@ def main():
         profile_1 = parse_profile(profile_file_1)
         profile_2 = parse_profile(profile_file_2)
 
-        
         try:
             assert np.all(profile_1[:, 0] == profile_2[:, 0])
         except:
@@ -275,8 +277,6 @@ def main():
         cut_points, sgRNA_intervals = load_cut_points_sgRNA_intervals(
             args.crispresso_output_folder_1
         )
-
-        
 
         # Quantification comparison barchart
         fig = plt.figure(figsize=(30, 15))
@@ -292,12 +292,12 @@ def main():
                 ]
             )
             / N_TOTAL_1
-            * 100.
+            * 100.0
         )
         means_sample_2 = (
             np.array([N_UNMODIFIED_2, N_MODIFIED_2, N_REPAIRED_2, N_MIXED_HDR_NHEJ_2])
             / N_TOTAL_2
-            * 100.
+            * 100.0
         )
 
         ax1 = fig.add_subplot(1, 2, 1)
@@ -353,6 +353,36 @@ def main():
         if args.save_also_png:
             plt.savefig(_jp("1.Comparison_Efficiency.png"), bbox_inches="tight")
 
+        ##### Plotly
+        means_sample = {}
+        labels = ["Unmodified", "NHEJ", "HDR", "Mixed"]
+        means_sample["Sample"] = [args.sample_1_name] * len(labels) + [
+            args.sample_2_name
+        ] * len(labels)
+
+        means_sample["Type"] = labels + labels
+
+        counts1 = []
+        counts2 = []
+        for idx in range(len(labels)):
+
+            counts1 += [means_sample_1[idx]]
+            counts2 += [means_sample_2[idx]]
+
+        means_sample["% Sequences"] = counts1 + counts2
+
+        df = pd.DataFrame.from_dict(means_sample)
+
+        df.to_csv(_jp("difference_in_counts.csv"))
+
+        fig = px.bar(df, x="Type", y="% Sequences", color="Sample", barmode="group")
+
+        fig.write_image(_jp("difference_in_counts.png"))
+
+        fig.write_html(_jp("difference_in_counts.html"))
+
+        #### Plotly
+
         # profile comparion
         fig1 = plt.figure(figsize=(20, 10))
 
@@ -366,7 +396,7 @@ def main():
             lw=4,
             label="%s combined mutations" % args.sample_1_name,
         )
-        
+
         plt.plot(
             effect_vector_any_2,
             color=(1, 0, 0, 0.3),
@@ -380,8 +410,11 @@ def main():
         ttest_results = ttest_ind(effect_vector_any_1, effect_vector_any_2)
         print(f"Compare the distributions (percentage shift) = {ttest_results}")
 
-
-        plt.text(5, y_max-10, f"Offset  p = {mannwhitney_results[1]:.3f}\nHeight p = {ttest_results[1]:.3f}")
+        plt.text(
+            5,
+            y_max - 10,
+            f"Offset  p = {mannwhitney_results[1]:.3f}\nHeight p = {ttest_results[1]:.3f}",
+        )
 
         if cut_points:
             for idx, cut_point in enumerate(cut_points):
@@ -528,7 +561,89 @@ def main():
                 bbox_inches="tight",
             )
 
-        
+        #### Plotly
+        df_effects1 = pd.DataFrame()
+
+        df_effects1["% Sequences"] = effect_vector_any_1
+        df_effects1["Reference amplicon position (bp)"] = np.arange(
+            len(effect_vector_any_1)
+        )
+        df_effects1["Sample"] = f"{args.sample_1_name} combined mutations"
+
+        df_effects2 = pd.DataFrame()
+
+        df_effects2["% Sequences"] = effect_vector_any_2
+        df_effects2["Reference amplicon position (bp)"] = np.arange(
+            len(effect_vector_any_2)
+        )
+        df_effects2["Sample"] = f"{args.sample_2_name} combined mutations"
+
+        df_effects = pd.concat([df_effects1, df_effects2])
+
+        df_effects.to_csv(_jp("compare_mutations.csv"))
+
+        fig1 = px.line(
+            df_effects,
+            x="Reference amplicon position (bp)",
+            y="% Sequences",
+            color="Sample",
+        )
+        fig = go.Figure(data=fig1.data)
+
+        if cut_points:
+            for idx, cut_point in enumerate(cut_points):
+                fig.add_vline(
+                    x=cut_point,
+                    line_width=3,
+                    line_dash="dash",
+                    line_color="green",
+                    annotation_text="Cut site",
+                )
+
+        mannwhitney_results = mannwhitneyu(effect_vector_any_1, effect_vector_any_2)
+        print(f"Compare the distributions (position shift)= {mannwhitney_results}")
+
+        ttest_results = ttest_ind(effect_vector_any_1, effect_vector_any_2)
+        print(f"Compare the distributions (percentage shift) = {ttest_results}")
+
+        fig.add_annotation(
+            x=30,
+            y=20,
+            text=f"Offset  p = {mannwhitney_results[1]:.3f}<br>Height p = {ttest_results[1]:.3f}",
+            showarrow=False,
+        )
+
+        for idx, sgRNA_int in enumerate(sgRNA_intervals):
+            if idx == 0:
+                fig.add_scatter(
+                    x=[sgRNA_int[0], sgRNA_int[1]],
+                    y=[0.5, 0.5],
+                    mode = "lines",
+                    marker=dict(
+                        color="grey",
+                        size=10,
+                        opacity=0.5,
+                    ),
+                    name="sgRNA guide"
+                )
+            else:
+                fig.add_scatter(
+                    x=[sgRNA_int[0], sgRNA_int[1]],
+                    y=[0.5, 0.5],
+                    mode="lines",
+                    marker=dict(
+                        color="grey",
+                        size=10,
+                        opacity=0.5,
+                    ),
+                    name="sgRNA guide",
+                    showlegend=False
+                )
+
+        fig.write_image(_jp("compare_plots.png"))
+
+        fig.write_html(_jp("compare_plots.html"))
+
         info("All Done!")
         print(
             """     
